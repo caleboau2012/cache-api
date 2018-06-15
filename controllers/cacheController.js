54107f64610af556877324f094348998add05d9c
KayLee <caleboau2012@gmail.com>
2018-06-15 10:02:25 +0100

    Added routes and dummy methods in controller to match specification.

---
+++ controllers/cacheController.js
@@ -0,0 +1,29 @@
+/**
+ * Created by KayLee on 15/06/2018.
+ */
+var Cache = require('../models/Cache');
+
+// Display all keys in cache.
+exports.getAll = function(req, res) {
+
+};
+
+// Get Data for a give key
+exports.getData = function(req, res){
+
+};
+
+// Create or Update Data
+exports.setData = function(req, res){
+    res.send("To be completed")
+};
+
+// Delete all data
+exports.deleteAll = function(req, res){
+    res.send("To be completed")
+};
+
+// Delete specific cache data
+exports.deleteData = function(req, res){
+    res.send("To be completed")
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var cacheController = require("../controllers/cacheController");
+
+/* GET routes. */
+router.get('/data', cacheController.getAll);
+router.get('/data/:key', cacheController.getData);
+
+/* POST Routes. */
+router.post('/data/:key', cacheController.setData);
+
+/* DELETE ROUTES. */
+router.delete('/data', cacheController.deleteAll);
+router.delete('/data/:key', cacheController.deleteData);
+
 
 module.exports = router;
